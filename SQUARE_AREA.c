0965221baa4acbb40fed48cac24574d91ca3feb8
angad <angadreact01@gmail.com>
2021-12-30 09:24:44 -0700

    first commit

---
+++ SQUARE_AREA.c
@@ -0,0 +1,12 @@
+// SQUARE AREA
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int side;
+    printf("Enter your side value\n");
+    scanf("%d", &side);
+    printf("Your area is %f\n", pow(side, 2));
+    return 0;
+}
